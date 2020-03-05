0d850b29869706d9f394284fa32f4bc09323ffeb
hongtao <935245421@qq.com>
2020-03-05 14:46:58 +0800

    import BaseContract to use MVP

---
+++ app/src/main/java/com/hongtao/live/base/BaseContract.java
@@ -0,0 +1,16 @@
+package com.hongtao.live.base;
+
+/**
+ * Created 2020/3/5.
+ *
+ * @author HongTao
+ */
+public class BaseContract {
+    public interface BaseView {
+        void setPresenter(BasePresenter presenter);
+    }
+
+    public interface BasePresenter{
+
+    }
+}
