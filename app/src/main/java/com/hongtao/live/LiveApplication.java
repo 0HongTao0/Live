8a0eab55d4809519e0772f8824690ebdb576b109
hongtao <935245421@qq.com>
2020-03-05 21:07:47 +0800

    import LiveApplication to replace default Application.

---
+++ app/src/main/java/com/hongtao/live/LiveApplication.java
@@ -0,0 +1,25 @@
+package com.hongtao.live;
+
+import android.app.Application;
+import android.content.Context;
+
+/**
+ * Created 2020/3/5.
+ *
+ * @author HongTao
+ */
+public class LiveApplication extends Application {
+    private static final String TAG = LiveApplication.class.getSimpleName();
+
+    private static Context sContext;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        sContext = getApplicationContext();
+    }
+
+    public static Context getContext() {
+        return sContext;
+    }
+}
